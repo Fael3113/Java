88ade7df2e58d6b0f39c0ed551de3f4d0bf9bd6d
Fael3113 <santiagorafaelsantiago@hotmail.com>
2025-02-11 15:17:39 -0300

    Outra remoção

--- Encurtou/src/main/java/dev/rafael/Encurtou/Links/LinkRepository.java
+++
@@ -1,8 +0,0 @@
-package dev.rafael.Encurtou.Links;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface LinkRepository extends JpaRepository<Link, Long> {
-	//Encontrar Urls Encurtadas
-	Link findByUrlEncurtada(String urlEncurtada);
-}
